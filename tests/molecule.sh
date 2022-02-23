6e1ee7e89662dabbf9f53a837996c887a660ce1d
Evgeni Golov <evgeni@golov.de>
2022-02-23 18:51:34 +0100

    add GHA group markers
    as suggested by Ewoud

--- tests/molecule.sh
+++ tests/molecule.sh
@@ -5,9 +5,11 @@ failed_roles=()
 for roledir in roles/*/molecule; do
   role_path=$(dirname $roledir)
   role_name=$(basename $role_path)
+  echo "::group::Ansible role $role_name"
   pushd ${role_path}
   molecule test --all || failed_roles+=($role_name)
   popd
+  echo "::endgroup::"
 done
 
 if [[ ${#failed_roles[@]} -ne 0 ]]; then
